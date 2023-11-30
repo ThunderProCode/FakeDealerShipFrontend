54f2af9e9160bf1ece60086fb570a0a9582d9984
Hector Acosta <hector.acosta@students.snow.edu>
2023-11-30 11:27:02 -0700

    GH Pages

---
+++ docs/precache-manifest.755db927c9c3ca68af7344fd8a916313.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6adf7b9f5d6727a72e249a5a96b77636",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3641d767b5cbbee579a",
+    "url": "/static/css/main.31ba5cb0.chunk.css"
+  },
+  {
+    "revision": "17007c9d57b3abf53349",
+    "url": "/static/js/2.b8617e57.chunk.js"
+  },
+  {
+    "revision": "b3641d767b5cbbee579a",
+    "url": "/static/js/main.578214cd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c522e1368e1f09868196f357a5c286a4",
+    "url": "/static/media/Logo.c522e136.svg"
+  },
+  {
+    "revision": "df5285f39764947acea88c75f0cfd0db",
+    "url": "/static/media/background.df5285f3.jpg"
+  }
+]);
