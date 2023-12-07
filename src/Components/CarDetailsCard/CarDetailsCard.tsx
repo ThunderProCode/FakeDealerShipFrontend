05829ef5b2fd01c7987bc98ce07c8de53f4153ee
thunderprocode <hectora887ju@gmail.com>
2023-12-07 01:39:19 -0600

    Add Switch Images Feature on CarDetailsCard component

--- src/Components/CarDetailsCard/CarDetailsCard.tsx
+++ src/Components/CarDetailsCard/CarDetailsCard.tsx
@@ -1,18 +1,36 @@
-import React from 'react';
+import React,{ useState } from 'react';
 import './CarDetailsCard.css';
 import { ICar } from '../../interfaces/ICar.Interface';
 import LikeButton from '../LikeButton/LikeButton';
 import { FaCheckSquare } from "react-icons/fa";
+import { MdNavigateNext, MdNavigateBefore } from "react-icons/md";
 
 interface CarDetailsCardProps {
     car:ICar;
 }
 
 const CarDetailsCard:React.FC<CarDetailsCardProps> = (props):JSX.Element => {
+
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+    const nextImage = () => {
+      setCurrentImageIndex((prevIndex) =>
+        prevIndex === props.car.images.length - 1 ? 0 : prevIndex + 1
+      );
+    };
+  
+    const prevImage = () => {
+      setCurrentImageIndex((prevIndex) =>
+        prevIndex === 0 ? props.car.images.length - 1 : prevIndex - 1
+      );
+    };
+
+    
     return(
         <div className="car-details-card">
-            <section className="details-card-image">
-                <img src={props.car.images[0]} alt="Car Image" />
+            <section className="details-card-image" style={{backgroundImage: `url(${props.car.images[currentImageIndex]})`}}>
+                <button className="details-card-prev-btn" onClick={prevImage}> <MdNavigateBefore/> </button>
+                <button className="details-card-next-btn" onClick={nextImage}> <MdNavigateNext/></button>
             </section>
             <section className="details-card-header">
                 <h2 className="details-card-title">{`${props.car.make} ${props.car.model}`}</h2>
