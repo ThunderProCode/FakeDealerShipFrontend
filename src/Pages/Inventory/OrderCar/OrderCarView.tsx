71d6e0abf0402e5601b19a42ffd488e8185d73a9
thunderprocode <hectora887ju@gmail.com>
2023-12-09 13:59:45 -0600

    Add Car Order Form Responsive Styles

--- src/Pages/Inventory/OrderCar/OrderCarView.tsx
+++ src/Pages/Inventory/OrderCar/OrderCarView.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React from "react";
 import './OrderCarView.css';
 import DropDownMenu from "../../../Components/DropDownMenu/DropDownMenu.Component";
 import { SiCashapp } from "react-icons/si";
